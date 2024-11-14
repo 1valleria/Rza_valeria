2aa8347f5472b910d3760e69e60685f784453947
Valeria Lupanciuc <lupanciucvaleria9@gmail.com>
2024-11-14 10:05:20 +0000

    Added the Bookings Page, Room bookingService and RoomService and a new models: Room and Roombooking

--- Rza_valeria/Models/Roombooking.cs
+++ Rza_valeria/Models/Roombooking.cs
@@ -1,23 +1,19 @@
-﻿//Import necessary namespaces for basic functionalities and collections.
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Rza_valeria.Models;
-//Defines the `Roombooking` class, representing a booking made by a customer for a hotel room
+
 public partial class Roombooking
 {
-    public int CustomerId { get; set; } // ID of the customer making the booking.
+    public int CustomerId { get; set; }
 
-    public int RoomNumber { get; set; }//Number of the hotel room being booked.
+    public int RoomNumber { get; set; }
 
-    public DateOnly StartDate { get; set; }//Start date of the room booking.
+    public DateOnly StartDate { get; set; }
 
-    public DateOnly? EndDate { get; set; }//Optional end date for the booking; nullable for open-ended bookings.
-   
-    // Navigation property representing the customer who made the booking.
+    public DateOnly? EndDate { get; set; }
+
     public virtual Customer Customer { get; set; } = null!;
 
-
-    // Navigation property to the `Hotelroom` associated with this booking, mapped by `RoomNumber`.
-    public virtual Hotelroom RoomNumberNavigation { get; set; } = null!;
+    public virtual Room Room { get; set; } = null!;
 }
