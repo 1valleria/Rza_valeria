5f54ff51b466db704498dd029796f2927472e007
Valeria Lupanciuc <S2301890@ndonline.ac.uk>
2024-11-18 12:48:13 +0000

    Added a new razor page - Tickets Page

--- Rza_valeria/Services/AttractionService.cs
+++ Rza_valeria/Services/AttractionService.cs
@@ -1,6 +1,18 @@
-﻿namespace Rza_valeria.Services
+﻿using Rza_valeria.Models;
+
+namespace Rza_valeria.Services
+
 {
     public class AttractionService
     {
+        private readonly RzaContext _context;
+        public AttractionService(RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Attraction>> GetAttractionsAsync()
+        {
+            return await _context.Attractions.ToListAsync();
+        }
     }
 }
