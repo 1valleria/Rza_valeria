62f4e5e55f855f151dd822013c3ebf7d30bc1c9c
Valeria Lupanciuc <S2301890@ndonline.ac.uk>
2024-11-19 09:23:29 +0000

    Added a new service - Ticket Service

---
+++ Rza_valeria/Services/TicketService.cs
@@ -0,0 +1,19 @@
+﻿using Rza_valeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rza_valeria.Services
+
+{
+    public class TicketService
+    {
+        private readonly RzaContext _context;
+        public TicketService(RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Attraction>> GetAttractionsAsync()
+        {
+            return await _context.Attractions.ToListAsync();
+        }
+    }
+}
